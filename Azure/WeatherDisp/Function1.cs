1311ffe49a1892ab93dce906124245244e030240
myst <mystster@gmail.com>
2020-02-21 05:20:49 +0900

    add puppeteer

--- Azure/WeatherDisp/Function1.cs
+++ Azure/WeatherDisp/Function1.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using PuppeteerSharp;
 
 namespace WeatherDisp
 {
@@ -14,20 +15,42 @@ namespace WeatherDisp
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            //string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            //return name != null
+            //   ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            //   : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions()
+            {
+                Headless = true,
+                LogProcess = true,
+                Args = new[]
+                {
+                    "--no-sandbox"
+                }
+            }))
+            {
+                using (var page = await browser.NewPageAsync())
+                {
+                    await page.SetViewportAsync(new ViewPortOptions()
+                    {
+                        Width = 298,
+                        Height = 128
+                    });
+                    await page.GoToAsync("https://www.google.com");
+                    return new FileContentResult(await page.ScreenshotDataAsync(), "image/jpeg");
+                }
+            }
         }
     }
 }
