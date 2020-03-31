674835912a9e86a557eb1b7362fd94bb9dbf2fe2
myst <mystster@gmail.com>
2020-03-31 20:30:48 +0000

    add weather component

--- Azure/Vue/weatherdisp/src/App.vue
+++ Azure/Vue/weatherdisp/src/App.vue
@@ -1,29 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div id="weatherToday">
+      aaaa
+      <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    </div>
+    <div id="weatherTommorow">
+      <Weather/>
+    </div>
+    <div id="tempToday">
+      ccccc
+    </div>
+    <div id="rainyToday">
+      dddddd
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Weather from './components/Weather.vue';
 
 @Component({
   components: {
-    HelloWorld
+    Weather
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+body, html{
+  width: 100%;
+  height: 100%;
+}
+body{
+  margin: 0;
+}
+</style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+  display: grid;
+  grid-template-columns: 50% 10% 40%;
+  grid-template-rows: 60% 40%;
 }
+#app > * {
+  border: 2px;
+}
+#weatherToday{
+  grid-column: 1 / 3;
+  grid-row: 1;
+  background: orange;
+}
+#weatherTommorow{
+  grid-column: 3;
+  grid-row: 1;
+  background: skyblue;
+}
+#tempToday{
+  grid-column: 1;
+  grid-row: 2;
+  background: violet;
+}
+#rainyToday{
+  grid-column: 2 / 4;
+  grid-row: 2;
+  background:yellow;
+}
+
 </style>
---
+++ Azure/Vue/weatherdisp/src/components/Weather.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="weather">
+      <div id="date"></div>
+      <div id="icon">
+        <skycon condition="rain" width="40" height="40"></skycon>
+      </div>
+      <div id="maxTempLabel">最高気温</div>
+      <div id="minTempLabel">最低気温</div>
+      <div id="rainyLabel">降水確率</div>
+      <div id="windLabel">風速</div>
+      <div id="maxTemp"></div>
+      <div id="minTemp"></div>
+      <div id="rainy"></div>
+      <div id="wind"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+// import {DayForecast} from 'darkskyapi-ts'
+
+@Component
+export default class Weather extends Vue {
+//  @Prop() private weatherInfo: DayForecast;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
