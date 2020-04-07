07d0a31e13e2737110a7eab35c0a2d2756dfbf87
myst <mystster@gmail.com>
2020-04-07 20:42:31 +0000

    work weather component

--- Azure/Vue/weatherdisp/src/App.vue
+++ Azure/Vue/weatherdisp/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
+  <div id="app" v-if="result">
     <div id="weatherToday">
-      aaaa
+      <Weather :dailyWeather="result.daily.data[0]"/>
       <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
     </div>
     <div id="weatherTommorow">
-      <Weather/>
+      <Weather :dailyWeather="result.daily.data[1]"/>
     </div>
     <div id="tempToday">
       ccccc
     </div>
     <div id="rainyToday">
-      dddddd
+      ddddddeee
     </div>
   </div>
 </template>
@@ -20,22 +20,46 @@
 import { Component, Vue } from "vue-property-decorator";
 // import HelloWorld from "./components/HelloWorld.vue";
 import Weather from './components/Weather.vue';
+import {DarkSkyClient, DarkSkyOptions, Units, Language, DarkSky, Forecast} from "darkskyapi-ts";
 
 @Component({
   components: {
     Weather
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  result:Forecast | false = false;
+  async created(){
+    const KEY = 'xxxxxxxx';
+    const lat = 1110;
+    const lng = 222;
+    // https://api.darksky.net/forecast/xxxxxxxx/1110,222
+    const options : DarkSkyOptions = {
+      units: Units.SI,
+      lang: Language.JAPANESE
+    }
+    const client = new DarkSky(KEY, options);
+
+    this.result = await client.chain(lat, lng)
+    .excludeFlags()
+    .excludeAlerts()
+    .excludeMinutely()
+    .execute()
+    
+    console.log(this.result);
+
+  }
+}
 </script>
 
 <style>
 body, html{
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 }
 body{
   margin: 0;
+  background: pink;
 }
 </style>
 <style scoped>
@@ -48,18 +72,20 @@ body{
   margin-top: 60px; */
   display: grid;
   grid-template-columns: 50% 10% 40%;
-  grid-template-rows: 60% 40%;
+  grid-template-rows: 70% auto;
+  width: 100vw;
+  height: 100vh;
 }
 #app > * {
   border: 2px;
 }
 #weatherToday{
-  grid-column: 1 / 3;
+  grid-column: 1 / 2;
   grid-row: 1;
   background: orange;
 }
 #weatherTommorow{
-  grid-column: 3;
+  grid-column: 2 / 4;
   grid-row: 1;
   background: skyblue;
 }
--- Azure/Vue/weatherdisp/src/components/Weather.vue
+++ Azure/Vue/weatherdisp/src/components/Weather.vue
@@ -1,44 +1,83 @@
 <template>
-  <div class="weather">
-      <div id="date"></div>
+  <div class="weather" id="weatherPane">
+      <div id="date">{{new Date(dailyWeather.time*1000).toLocaleDateString("ja-JP",{month: 'numeric', day: 'numeric' })}}</div>
       <div id="icon">
-        <skycon condition="rain" width="40" height="40"></skycon>
+        <skycon :condition="dailyWeather.icon" width="60" height="60"></skycon>
       </div>
-      <div id="maxTempLabel">最高気温</div>
-      <div id="minTempLabel">最低気温</div>
-      <div id="rainyLabel">降水確率</div>
-      <div id="windLabel">風速</div>
-      <div id="maxTemp"></div>
-      <div id="minTemp"></div>
-      <div id="rainy"></div>
-      <div id="wind"></div>
+      <div id="maxTempLabel">高</div>
+      <div id="minTempLabel">低</div>
+      <div id="rainyLabel">雨</div>
+      <div id="windLabel">風</div>
+      <div id="maxTemp">{{Math.round(dailyWeather.temperatureHigh)}}℃</div>
+      <div id="minTemp">{{Math.round(dailyWeather.temperatureLow)}}℃</div>
+      <div id="rainy">{{Math.round(dailyWeather.precipProbability*10)*10}}%</div>
+      <div id="wind">{{Math.round(dailyWeather.windSpeed)}}m/s</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-// import {DayForecast} from 'darkskyapi-ts'
+import {Forecast, DataBlock, } from 'darkskyapi-ts'
 
 @Component
 export default class Weather extends Vue {
-//  @Prop() private weatherInfo: DayForecast;
+  @Prop() private dailyWeather?: DataBlock
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#weatherPane {
+  display: grid;
+  grid-template-columns: 1.5fr 0.5fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  height: 100%;
+  margin: 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#weatherPane > * {
+  margin: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#date{
+  grid-column: 1;
+  grid-row: 1;
 }
-a {
-  color: #42b983;
+#icon{
+  grid-column: 1;
+  grid-row: 2 / 4;
+}
+#maxTempLabel{
+  grid-column: 2;
+  grid-row: 1;
+  font-size-adjust: inherit;
+}
+#minTempLabel{
+  grid-column: 2;
+  grid-row: 2;
+}
+#rainyLabel{
+  grid-column: 2;
+  grid-row: 3;
+}
+#windLabel{
+  grid-column: 2;
+  grid-row: 4;
+}
+#maxTemp{
+  grid-column: 3;
+  grid-row: 1;
+  font-size:100%;
+}
+#minTemp{
+  grid-column: 3;
+  grid-row: 2;
+}
+#rainy{
+  grid-column: 3;
+  grid-row: 3;
+}
+#wind{
+  grid-column: 3;
+  grid-row: 4;
 }
 </style>
