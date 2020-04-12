a480337dfa19d4acd80cf2e01d88810e53f02ff8
myst <mystster@gmail.com>
2020-04-12 21:08:32 +0000

    lint

--- Azure/Vue/weatherdisp/src/typings/vue-skycon.d.ts
+++ Azure/Vue/weatherdisp/src/typings/vue-skycon.d.ts
@@ -1,4 +1,4 @@
-import { PluginObject } from 'vue';
+import { PluginObject } from "vue";
 declare module "vue-skycon" {
-    export default function VueSkycons() : PluginObject<unknown>;
+  export default function VueSkycons(): PluginObject<unknown>;
 }
