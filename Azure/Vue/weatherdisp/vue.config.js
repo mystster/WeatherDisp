43e5a483d7a46b56726b9b21773a69970cd1d739
myst <mystster@gmail.com>
2020-04-12 21:07:49 +0000

    use relative path when production

--- Azure/Vue/weatherdisp/vue.config.js
+++ Azure/Vue/weatherdisp/vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '.'
+        : '/',
     devServer: {
         watchOptions: {
             poll: true
         }
     }
-}
+};
