158f0b41f4e1d465ed0b4633996f322c9522790c
myst <mystster@gmail.com>
2020-04-23 13:41:45 +0000

    fix useColor() error on browser

--- Azure/Vue/weatherdisp/vue.config.js
+++ Azure/Vue/weatherdisp/vue.config.js
@@ -6,5 +6,39 @@ module.exports = {
         watchOptions: {
             poll: true
         }
+    },
+    css: {
+        extract: false,
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        },
+    },
+    chainWebpack: config => {
+        config.plugins.delete('prefetch')
+        config.plugins.delete('preload')
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].inlineSource = '.(js|css|map)$';
+                return args
+            })
+        config
+            .plugin('html-inline-source')
+            .use(require('html-webpack-inline-source-plugin'))
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .options({})
+        config.module
+            .rule('replace')
+            .test(/.*darksky\-api\.es5\.js$/)
+            .use('string-replace-loader')
+            .loader('string-replace-loader')
+            .options({
+                search: /return 'colors' in exports\.inspectOpts\s+\? Boolean\(exports\.inspectOpts\.colors\)\s+: tty\.isatty\(process\.stderr\.fd\);/gim,
+                replace: 'return false'
+            })
     }
 };
