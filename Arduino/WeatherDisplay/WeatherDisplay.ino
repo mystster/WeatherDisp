40dbf49b5b885a9dd4033d8fb34608fdf8b1bcc9
myst <mystster@gmail.com>
2020-05-05 06:09:07 +0900

    remove unsed comments

--- Arduino/WeatherDisplay/WeatherDisplay.ino
+++ Arduino/WeatherDisplay/WeatherDisplay.ino
@@ -1,68 +1,10 @@
-
-// GxEPD_Example : test example for e-Paper displays from Waveshare and from Dalian Good Display Inc.
-//
-// Created by Jean-Marc Zingg based on demo code from Good Display,
-// available on http://www.e-paper-display.com/download_list/downloadcategoryid=34&isMode=false.html
-//
-// The e-paper displays are available from:
-//
-// https://www.aliexpress.com/store/product/Wholesale-1-54inch-E-Ink-display-module-with-embedded-controller-200x200-Communicate-via-SPI-interface-Supports/216233_32824535312.html
-//
-// http://www.buy-lcd.com/index.php?route=product/product&path=2897_8363&product_id=35120
-// or https://www.aliexpress.com/store/product/E001-1-54-inch-partial-refresh-Small-size-dot-matrix-e-paper-display/600281_32815089163.html
-//
-
-// Supporting Arduino Forum Topics:
-// Waveshare e-paper displays with SPI: http://forum.arduino.cc/index.php?topic=487007.0
-// Good Dispay ePaper for Arduino : https://forum.arduino.cc/index.php?topic=436411.0
-
 // mapping suggestion from Waveshare SPI e-Paper to Wemos D1 mini
 // BUSY -> D2, RST -> D4, DC -> D3, CS -> D8, CLK -> D5, DIN -> D7, GND -> GND, 3.3V -> 3.3V
 
-// mapping suggestion from Waveshare SPI e-Paper to generic ESP8266
-// BUSY -> GPIO4, RST -> GPIO2, DC -> GPIO0, CS -> GPIO15, CLK -> GPIO14, DIN -> GPIO13, GND -> GND, 3.3V -> 3.3V
-
-// mapping suggestion for ESP32, e.g. LOLIN32, see .../variants/.../pins_arduino.h for your board
-// NOTE: there are variants with different pins for SPI ! CHECK SPI PINS OF YOUR BOARD
-// BUSY -> 4, RST -> 16, DC -> 17, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V
-
-// new mapping suggestion for STM32F1, e.g. STM32F103C8T6 "BluePill"
-// BUSY -> A1, RST -> A2, DC -> A3, CS-> A4, CLK -> A5, DIN -> A7
-
-// mapping suggestion for AVR, UNO, NANO etc.
-// BUSY -> 7, RST -> 9, DC -> 8, CS-> 10, CLK -> 13, DIN -> 11
-
-// mapping suggestion for Arduino MEGA
-// BUSY -> 7, RST -> 9, DC -> 8, CS-> 53, CLK -> 52, DIN -> 51
-
-// mapping suggestion for Arduino DUE
-// BUSY -> 7, RST -> 9, DC -> 8, CS-> 77, CLK -> 76, DIN -> 75
-// SPI pins are also on 6 pin 2x3 SPI header
-
 // include library, include base class, make path known
 #include <GxEPD.h>
-
-// select the display class to use, only one
-//#include <GxGDEP015OC1/GxGDEP015OC1.h>    // 1.54" b/w
-//#include <GxGDEW0154Z04/GxGDEW0154Z04.h>  // 1.54" b/w/r 200x200
-//#include <GxGDEW0154Z17/GxGDEW0154Z17.h>  // 1.54" b/w/r 152x152
-//#include <GxGDEW0213I5F/GxGDEW0213I5F.h>  // 2.13" b/w 104x212 flexible
-//#include <GxGDE0213B1/GxGDE0213B1.h>      // 2.13" b/w
-//#include <GxGDEW0213Z16/GxGDEW0213Z16.h>  // 2.13" b/w/r
-//#include <GxGDEH029A1/GxGDEH029A1.h>      // 2.9" b/w
 #include <GxGDEW029T5/GxGDEW029T5.h> // 2.9" b/w IL0373
-//#include <GxGDEW029Z10/GxGDEW029Z10.h>    // 2.9" b/w/r
-//#include <GxGDEW027C44/GxGDEW027C44.h>    // 2.7" b/w/r
-//#include <GxGDEW027W3/GxGDEW027W3.h>      // 2.7" b/w
-//#include <GxGDEW042T2/GxGDEW042T2.h>      // 4.2" b/w
-//#include <GxGDEW042Z15/GxGDEW042Z15.h>    // 4.2" b/w/r
-//#include <GxGDEW0583T7/GxGDEW0583T7.h>    // 5.83" b/w
-//#include <GxGDEW075T8/GxGDEW075T8.h>      // 7.5" b/w
-//#include <GxGDEW075Z09/GxGDEW075Z09.h>    // 7.5" b/w/r
 
-//#include GxEPD_BitmapExamples
-
-// FreeFonts from Adafruit_GFX
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
 
@@ -75,8 +17,6 @@
 
 GxIO_Class io(SPI, /*CS=D8*/ D8, /*DC=D3*/ D3, /*RST=D4*/ D4); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
 GxEPD_Class display(io, /*RST=D4*/ D4, /*BUSY=D2*/ D2);        // default selection of D4(=2), D2(=4)
-// Heltec E-Paper 1.54" b/w without RST, BUSY
-//GxEPD_Class display(io, /*RST=D4*/ -1, /*BUSY=D2*/ -1); // no RST, no BUSY
 
 void setup()
 {
@@ -112,12 +52,9 @@ void setup()
     int16_t status = client.GET();
 
     Serial.println("Http get repuested");
-    // TODO: statusがマイナスになることがある
     if (status == 200)
     {
       Serial.println("File GET");
-      // WiFiClient *stream = client.getStreamPtr();
-      // get available data size
 
       uint32_t size = client.getSize();
       Serial.printf("Memory size is %d\n", size);
